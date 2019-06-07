a7a90c2e73cb02aab4cddc4ee244b5ee4e772ef9
Matthias Grob <maetugr@gmail.com>
2019-06-07 12:29:03 +0100

    ControlMath: add thrust projection to body attitude
    This requires the planned attitude to not change faster than it can
    acutally be executed which is not implemented yet.

--- src/modules/mc_pos_control/PositionControl/ControlMath.cpp
+++ src/modules/mc_pos_control/PositionControl/ControlMath.cpp
@@ -56,9 +56,13 @@ vehicle_attitude_setpoint_s thrustToAttitude(const Vector3f &thr_sp, const float
 
 vehicle_attitude_setpoint_s accelerationToAttitude(const Vector3f &acc_sp, const float yaw_sp, const float hover_thrust)
 {
+	// Assume standard acceleration due to gravity in vertical direction for attitude generation
 	Vector3f body_z = Vector3f(-acc_sp(0), -acc_sp(1), A_GRAVITY);
 	vehicle_attitude_setpoint_s att_sp = bodyzToAttitude(body_z, yaw_sp);
+	// Scale thrust assuming hover thrust produces standard gravity
 	att_sp.thrust_body[2] = acc_sp(2) * (hover_thrust / A_GRAVITY) - hover_thrust;
+	// Project thrust to planned body attitude
+	att_sp.thrust_body[2] = att_sp.thrust_body[2] / (Vector3f(0, 0, 1).dot(body_z.normalized()));
 	return att_sp;
 }
 
