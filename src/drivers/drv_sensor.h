yusufmk/Firmware

WIP: Bosch BMI088 IMU

src/drivers/drv_sensor.h
@@ -113,6 +113,8 @@
 #define DRV_ACC_DEVTYPE_ADIS16497	0x63
 #define DRV_GYR_DEVTYPE_ADIS16497	0x64
 #define DRV_BARO_DEVTYPE_BAROSIM	0x65
+#define DRV_ACC_DEVTYPE_BMI088		0x66
+#define DRV_GYR_DEVTYPE_BMI088		0x67
 
 /*
  * ioctl() definitions

bigonestep/Takeoff-and-Landing-controller-for-PX4-Fixed-wing-aircraft

src/drivers/drv_sensor.h
@@ -114,6 +114,8 @@
 #define DRV_GYR_DEVTYPE_ADIS16497	0x64
 #define DRV_BARO_DEVTYPE_BAROSIM	0x65
 #define DRV_BARO_DEVTYPE_BMP388		0x66
+#define DRV_ACC_DEVTYPE_BMI088		0x67
+#define DRV_GYR_DEVTYPE_BMI088		0x68
 
 /*
  * ioctl() definitions