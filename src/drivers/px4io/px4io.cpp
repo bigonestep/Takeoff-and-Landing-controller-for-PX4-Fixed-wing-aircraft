913e5d11917f6c5cfe5dea8b36657eb4f14abdb5
Daniel Agar <daniel@agar.ca>
2019-08-24 19:52:11 -0400

    px4io delete PX4_FMU_V2 define

--- src/drivers/px4io/px4io.cpp
+++ src/drivers/px4io/px4io.cpp
@@ -1978,11 +1978,10 @@ PX4IO::io_reg_modify(uint8_t page, uint8_t offset, uint16_t clearbits, uint16_t 
 int
 PX4IO::print_debug()
 {
-#ifdef CONFIG_ARCH_BOARD_PX4_FMU_V2
 	int io_fd = -1;
 
 	if (io_fd <= 0) {
-		io_fd = ::open("/dev/ttyS0", O_RDONLY | O_NONBLOCK | O_NOCTTY);
+		io_fd = ::open(PX4IO_SERIAL_PORT, O_RDONLY | O_NONBLOCK | O_NOCTTY);
 	}
 
 	/* read IO's output */
@@ -2014,9 +2013,7 @@ PX4IO::print_debug()
 		return 0;
 	}
 
-#endif
 	return 1;
-
 }
 
 int
