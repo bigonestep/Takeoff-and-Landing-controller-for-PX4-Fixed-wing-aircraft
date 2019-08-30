4462538cc36b0a65e4ee7a599673c883476f6cff
Daniel Agar <daniel@agar.ca>
2019-08-30 12:28:10 -0400

    WIP: increase stack for stackcheck

--- src/modules/dataman/dataman.cpp
+++ src/modules/dataman/dataman.cpp
@@ -1448,7 +1448,7 @@ start()
 	px4_sem_setprotocol(&g_init_sema, SEM_PRIO_NONE);
 
 	/* start the worker thread with low priority for disk IO */
-	if ((task = px4_task_spawn_cmd("dataman", SCHED_DEFAULT, SCHED_PRIORITY_DEFAULT - 10, 1200, task_main, nullptr)) < 0) {
+	if ((task = px4_task_spawn_cmd("dataman", SCHED_DEFAULT, SCHED_PRIORITY_DEFAULT - 10, 1280, task_main, nullptr)) < 0) {
 		px4_sem_destroy(&g_init_sema);
 		PX4_ERR("task start failed");
 		return -1;
