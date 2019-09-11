0e20dd49dab05b23dea691a0e4295510ff0c7166
Daniel Agar <daniel@agar.ca>
2019-09-11 17:04:33 -0400

    ulanding: run in uart WQ

--- src/drivers/distance_sensor/ulanding/ulanding.cpp
+++ src/drivers/distance_sensor/ulanding/ulanding.cpp
@@ -172,7 +172,7 @@ private:
 
 Radar::Radar(const char *port, uint8_t rotation) :
 	CDev(RANGE_FINDER0_DEVICE_PATH),
-	ScheduledWorkItem(px4::wq_configurations::hp_default),
+	ScheduledWorkItem(px4::serial_port_to_wq(port)),
 	_rotation(rotation)
 {
 	/* store port name */
