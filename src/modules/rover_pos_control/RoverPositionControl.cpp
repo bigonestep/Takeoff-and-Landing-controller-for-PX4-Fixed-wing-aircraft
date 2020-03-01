83f334cebb1f5d6419887654da92cb141646e034
JaeyoungLim <gokoreas@gmail.com>
2020-03-01 19:18:37 +0100

    Subscribe to vehicle rates and rate setpoints
    This creates the data pipeline for offboard rate setpoints
    Set rates to attitude updates
    
    
    Subscribe to rate setpoints properly
    
    
    Working

--- src/modules/rover_pos_control/RoverPositionControl.cpp
+++ src/modules/rover_pos_control/RoverPositionControl.cpp
@@ -148,6 +148,28 @@ RoverPositionControl::vehicle_attitude_poll()
 	}
 }
 
+void
+RoverPositionControl::rates_setpoint_poll()
+{
+	bool rates_sp_updated;
+	orb_check(_rates_sp_sub, &rates_sp_updated);
+
+	if (rates_sp_updated) {
+		orb_copy(ORB_ID(vehicle_rates_setpoint), _rates_sp_sub, &_rates_sp);
+	}
+}
+
+void
+RoverPositionControl::vehicle_rates_poll()
+{
+	bool rates_updated;
+	orb_check(_vehicle_rates_sub, &rates_updated);
+
+	if (rates_updated) {
+		orb_copy(ORB_ID(vehicle_angular_velocity), _vehicle_rates_sub, &_vehicle_rate);
+	}
+}
+
 bool
 RoverPositionControl::control_position(const matrix::Vector2f &current_position,
 				       const matrix::Vector3f &ground_speed, const position_setpoint_triplet_s &pos_sp_triplet)
@@ -348,6 +370,30 @@ RoverPositionControl::control_attitude(const vehicle_attitude_s &att, const vehi
 }
 
 void
+RoverPositionControl::control_rates(const vehicle_angular_velocity_s &rates, const vehicle_rates_setpoint_s &rates_sp)
+{
+	//TODO: Add PID for rate controls
+	PX4_INFO("controlling rates");
+	float control_effort = rates_sp.yaw; //Should I make control effort a global?
+	control_effort = math::constrain(control_effort, -1.0f, 1.0f);
+
+	const float control_throttle = math::constrain(rates_sp.thrust_body[0], -1.0f, 1.0f);
+	PX4_INFO("  - thrust0: %f", double(rates_sp.thrust_body[0]));
+	PX4_INFO("  - rates : %f", double(control_effort));
+
+	if (control_throttle >= 0.0f) {
+		_act_controls.control[actuator_controls_s::INDEX_YAW] = control_effort;
+
+	} else {
+		// reverse steering, if driving backwards
+		_act_controls.control[actuator_controls_s::INDEX_YAW] = -control_effort;
+	}
+
+	_act_controls.control[actuator_controls_s::INDEX_THROTTLE] = control_throttle;
+
+}
+
+void
 RoverPositionControl::run()
 {
 	_control_mode_sub = orb_subscribe(ORB_ID(vehicle_control_mode));
@@ -356,8 +402,11 @@ RoverPositionControl::run()
 	_manual_control_sub = orb_subscribe(ORB_ID(manual_control_setpoint));
 	_pos_sp_triplet_sub = orb_subscribe(ORB_ID(position_setpoint_triplet));
 	_att_sp_sub = orb_subscribe(ORB_ID(vehicle_attitude_setpoint));
+	_rates_sp_sub = orb_subscribe(ORB_ID(vehicle_rates_setpoint));
 
 	_vehicle_attitude_sub = orb_subscribe(ORB_ID(vehicle_attitude));
+	_vehicle_rates_sub = orb_subscribe(ORB_ID(vehicle_angular_velocity));
+
 	_sensor_combined_sub = orb_subscribe(ORB_ID(sensor_combined));
 
 	/* rate limit control mode updates to 5Hz */
@@ -385,7 +434,7 @@ RoverPositionControl::run()
 	while (!should_exit()) {
 
 		/* wait for up to 500ms for data */
-		int pret = px4_poll(&fds[0], (sizeof(fds) / sizeof(fds[0])), 500);
+		int pret = px4_poll(&fds[3], (sizeof(fds) / sizeof(fds[3])), 500);
 
 		/* this is undesirable but not much we can do - might want to flag unhappy status */
 		if (pret < 0) {
@@ -396,6 +445,7 @@ RoverPositionControl::run()
 		/* check vehicle control mode for changes to publication state */
 		vehicle_control_mode_poll();
 		attitude_setpoint_poll();
+		rates_setpoint_poll();
 		//manual_control_setpoint_poll();
 
 		_vehicle_acceleration_sub.update();
@@ -477,12 +527,20 @@ RoverPositionControl::run()
 		if (fds[3].revents & POLLIN) {
 
 			vehicle_attitude_poll();
+			vehicle_rates_poll();
 
 			if (!manual_mode && _control_mode.flag_control_attitude_enabled
 			    && !_control_mode.flag_control_position_enabled
 			    && !_control_mode.flag_control_velocity_enabled) {
 
 				control_attitude(_vehicle_att, _att_sp);
+
+			} else if (!manual_mode && _control_mode.flag_control_rates_enabled
+			    && !_control_mode.flag_control_attitude_enabled
+			    && !_control_mode.flag_control_position_enabled
+			    && !_control_mode.flag_control_velocity_enabled) {
+
+				control_rates(_vehicle_rate, _rates_sp);
 
 			}
 
@@ -504,16 +562,16 @@ RoverPositionControl::run()
 			}
 		}
 
-		if (fds[2].revents & POLLIN) {
+		if (fds[3].revents & POLLIN) {
 
 			orb_copy(ORB_ID(sensor_combined), _sensor_combined_sub, &_sensor_combined);
 
-			//orb_copy(ORB_ID(vehicle_attitude), _vehicle_attitude_sub, &_vehicle_att);
 			_act_controls.timestamp = hrt_absolute_time();
 
 			/* Only publish if any of the proper modes are enabled */
 			if (_control_mode.flag_control_velocity_enabled ||
 			    _control_mode.flag_control_attitude_enabled ||
+    			    _control_mode.flag_control_rates_enabled ||
 			    manual_mode) {
 				/* publish the actuator controls */
 				_actuator_controls_pub.publish(_act_controls);
@@ -527,7 +585,9 @@ RoverPositionControl::run()
 	orb_unsubscribe(_local_pos_sub);
 	orb_unsubscribe(_manual_control_sub);
 	orb_unsubscribe(_pos_sp_triplet_sub);
+	orb_unsubscribe(_rates_sp_sub);
 	orb_unsubscribe(_vehicle_attitude_sub);
+	orb_unsubscribe(_vehicle_rates_sub);
 	orb_unsubscribe(_sensor_combined_sub);
 
 	warnx("exiting.\n");
--- src/modules/rover_pos_control/RoverPositionControl.hpp
+++ src/modules/rover_pos_control/RoverPositionControl.hpp
@@ -63,11 +63,13 @@
 #include <uORB/topics/position_setpoint_triplet.h>
 #include <uORB/topics/sensor_combined.h>
 #include <uORB/topics/vehicle_acceleration.h>
+#include <uORB/topics/vehicle_angular_velocity.h>
 #include <uORB/topics/vehicle_attitude.h>
 #include <uORB/topics/vehicle_attitude_setpoint.h>
 #include <uORB/topics/vehicle_control_mode.h>
 #include <uORB/topics/vehicle_global_position.h>
 #include <uORB/topics/vehicle_local_position.h>
+#include <uORB/topics/vehicle_rates_setpoint.h>
 #include <uORB/topics/actuator_controls.h>
 #include <uORB/topics/ekf2_timestamps.h>
 #include <uORB/uORB.h>
@@ -108,14 +110,18 @@ private:
 	int		_manual_control_sub{-1};		/**< notification of manual control updates */
 	int		_pos_sp_triplet_sub{-1};
 	int		_att_sp_sub{-1};
+	int		_rates_sp_sub{-1};
 	int		_vehicle_attitude_sub{-1};
+	int		_vehicle_rates_sub{-1};
 	int		_sensor_combined_sub{-1};
 
 	uORB::Subscription	_parameter_update_sub{ORB_ID(parameter_update)};
 
 	manual_control_setpoint_s		_manual{};			/**< r/c channel data */
 	position_setpoint_triplet_s		_pos_sp_triplet{};		/**< triplet of mission items */
+	vehicle_angular_velocity_s		_vehicle_rate{};			/**< angular rates > */
 	vehicle_attitude_setpoint_s		_att_sp{};			/**< attitude setpoint > */
+	vehicle_rates_setpoint_s		_rates_sp{};			/**< rate setpoint > */
 	vehicle_control_mode_s			_control_mode{};		/**< control mode */
 	vehicle_global_position_s		_global_pos{};			/**< global vehicle position */
 	vehicle_local_position_s		_local_pos{};			/**< global vehicle position */
@@ -176,8 +182,10 @@ private:
 	void		manual_control_setpoint_poll();
 	void		position_setpoint_triplet_poll();
 	void		attitude_setpoint_poll();
+	void		rates_setpoint_poll();
 	void		vehicle_control_mode_poll();
 	void 		vehicle_attitude_poll();
+	void		vehicle_rates_poll();
 
 	/**
 	 * Control position.
@@ -186,5 +194,6 @@ private:
 					 const position_setpoint_triplet_s &_pos_sp_triplet);
 	void		control_velocity(const matrix::Vector3f &current_velocity, const position_setpoint_triplet_s &pos_sp_triplet);
 	void		control_attitude(const vehicle_attitude_s &att, const vehicle_attitude_setpoint_s &att_sp);
+	void		control_rates(const vehicle_angular_velocity_s &rates, const vehicle_rates_setpoint_s &rates_sp);
 
 };
