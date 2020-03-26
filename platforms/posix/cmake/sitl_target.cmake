d4232da86a584d11cf82e4df09cdbb66557cbd4f
JaeyoungLim <gokoreas@gmail.com>
2020-03-26 17:02:15 +0100

    Add locations as cmake targets

--- platforms/posix/cmake/sitl_target.cmake
+++ platforms/posix/cmake/sitl_target.cmake
@@ -66,7 +66,7 @@ set(models none shell
 	standard_vtol tailsitter tiltrotor
 	rover boat
 	uuv_hippocampus)
-set(worlds none empty warehouse sonoma_raceway)
+set(worlds none empty warehouse sonoma_raceway baylands yosemite mcmillan_airfield)
 set(all_posix_vmd_make_targets)
 foreach(viewer ${viewers})
 	foreach(debugger ${debuggers})
